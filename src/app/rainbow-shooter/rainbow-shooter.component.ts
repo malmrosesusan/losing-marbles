bec5e299a057c981f05b1d2bf1d978400507869b
malmrosesusan <malmrosesusan@gmail.com>
2023-01-04 16:21:55 -0700

    changing square size

--- src/app/rainbow-shooter/rainbow-shooter.component.ts
+++ src/app/rainbow-shooter/rainbow-shooter.component.ts
@@ -25,12 +25,12 @@ class GameScene extends BaseScene {
     private width: number;
     private height: number;
     private nbBlockTypes: number = 10; // 7 possible tetrominoes
-    private blockSize: number = 16; // px
+    private blockSize: number = 32; // px
     private spriteGrid: Phaser.GameObjects.Sprite[] = [];
-    private numGridRows: number = 20;
+    private numGridRows: number = 10;
     private numBlackRows: number = 6;
-    private numRandomColorRows: number = 4;
-    private numGridCols: number = 20;
+    private numBottomColorRows: number = 4;
+    private numGridCols: number = 10;
     private columnsUsed: number[] = [];
 
     init() {
@@ -64,7 +64,8 @@ class GameScene extends BaseScene {
         this.load.image('color10-highlight', 'assets/rainbow-shooter/color10-highlight.png');
         this.load.image('player', 'assets/rainbow-shooter/player.png');
         this.load.image('bullet', 'assets/rainbow-shooter/player.png');
-        this.load.image('piece', 'assets/rainbow-shooter/black.png');
+        this.load.image('piece', 'assets/rainbow-shooter/black-dot.png');
+        this.load.image('blackbg-sprite', 'assets/rainbow-shooter/black.png');
     }
 
 
@@ -85,14 +86,13 @@ class GameScene extends BaseScene {
 
         this.createRows();
         // player
-        this.player = this.add.sprite(((this.numGridCols * this.blockSize) / 2) - this.blockSize, (this.numGridRows * this.blockSize) + this.blockSize, 'player').setOrigin(0, 0);
+        this.player = this.add.sprite(((this.numGridCols * this.blockSize) / 2) - this.blockSize, (this.numGridRows * this.blockSize) + this.blockSize, 'player').setOrigin(0, 0).setScale(2);
 
         this.addNewPlayerPiece();
 
-        // todo figure out how to prevent sprites from being in same column
         const column = this.getColumn();
-        const sprite = this.add.sprite(column, 0, 'piece').setOrigin(0, 0).setData({color: 0});
-        
+        const sprite = this.add.sprite(column, 0, 'piece').setOrigin(0, 0).setData({ color: 0 }).setScale(2);
+
         this.fallingPieces.push(sprite);
 
     }
@@ -101,17 +101,22 @@ class GameScene extends BaseScene {
         this.updateIterations++;
 
         // create falling pieces
-        if (this.updateIterations % 36 === 0) { // once every 36 updates
+        if (this.updateIterations % 72 === 0) { // once every 36 updates
 
-            if (this.fallingPieces.length < 6 && this.updateIterations % 72 === 0) {
+            if (this.fallingPieces.length < 4 && this.updateIterations % 288 === 0) {
 
                 // todo figure out how to prevent sprites from being in same column
                 const column = this.getColumn();
-                const sprite = this.add.sprite(column, 0, 'piece').setOrigin(0, 0);
+                const sprite = this.add.sprite(column, 0, 'piece').setOrigin(0, 0).setScale(2);
                 this.fallingPieces.push(sprite);
             }
             for (const sprite of this.fallingPieces) {
-                sprite.y = sprite.y + this.blockSize;
+                if (sprite.y/this.blockSize === this.numGridRows - 1) {
+                    sprite.destroy()
+                }
+                else {
+                    sprite.y = sprite.y + this.blockSize;
+                }
             }
 
 
@@ -184,18 +189,23 @@ class GameScene extends BaseScene {
         const y = this.roundDownNearest(sprite.y, this.blockSize) / this.blockSize;
         const newColorNumber = this.getNewColor(y);
         const newColor = this.getColorName(newColorNumber);
-        sprite.setTexture(newColor).setData({color: newColorNumber});
+        sprite.setTexture(newColor).setData({ color: newColorNumber });
+        console.log('numBottomColorRows',this.numBottomColorRows);
 
-        const spritesInRow = this.children.list.filter(spr => spr instanceof Phaser.GameObjects.Sprite).filter(spr => (spr as Phaser.GameObjects.Sprite).x === sprite.x && (spr as Phaser.GameObjects.Sprite).y > ((this.numBlackRows + this.numGridRows)-1) * this.blockSize);
-        const highestSpriteInRow = spritesInRow.filter(spr => (spr as Phaser.GameObjects.Sprite).y === Math.min(...spritesInRow.map(o => (o as Phaser.GameObjects.Sprite).y)));
-        let moveToY: number = (highestSpriteInRow[0] as Phaser.GameObjects.Sprite).y;
-        if (newColorNumber === highestSpriteInRow[0].getData('color')) {
-            highestSpriteInRow[0].destroy();
+
+        const spritesInCol = this.children.list.filter(spr => spr instanceof Phaser.GameObjects.Sprite).filter(spr => (spr as Phaser.GameObjects.Sprite).x === sprite.x && (spr as Phaser.GameObjects.Sprite).y > ((this.numBlackRows + this.numGridRows) - 1) * this.blockSize);
+        const highestSpriteInCol = spritesInCol.filter(spr => (spr as Phaser.GameObjects.Sprite).y === Math.min(...spritesInCol.map(o => (o as Phaser.GameObjects.Sprite).y)));
+        //let moveToY: number = (highestSpriteInCol[0] as Phaser.GameObjects.Sprite).y;
+        
+        let moveToY: number = (this.numGridRows + this.numBlackRows) * this.blockSize;
+       // console.log('movetoy', moveToY);
+        let destroyRow: boolean = false;
+        if (newColorNumber === highestSpriteInCol[0].getData('color')) {
+            destroyRow = true;
         }
         else {
-            moveToY = moveToY - this.blockSize
+            moveToY = moveToY - this.blockSize;
         }
-        
 
         this.tweens.add({
             targets: sprite,
@@ -203,19 +213,98 @@ class GameScene extends BaseScene {
             y: moveToY,
             ease: 'Linear',
             duration: 500,
-            onComplete: () => { this.addNewPlayerPiece(); this.firing = false; sprite.setTexture(`color${newColorNumber}`) }
+            onComplete: () => {
+                this.addNewPlayerPiece(); this.firing = false; sprite.setTexture(`color${newColorNumber}`);
+                this.handleBottomRows(moveToY, destroyRow, newColorNumber);
+            }
         });
 
         const column = this.getColumn();
-        this.fallingPieces.splice(index, 1, this.add.sprite(column, 0, 'piece').setOrigin(0, 0).setData({color: index}));
+        this.fallingPieces.splice(index, 1, this.add.sprite(column, 0, 'piece').setOrigin(0, 0).setData({ color: index }).setScale(2));
 
         this.bullet.destroy();
 
     }
-    
 
+    private handleBottomRows(yOffset: number, destroy: boolean, color: number): void {
+        if (this.numBlackRows === 1) {
+            console.log('GAME OVER');
+            this.scene.pause();
+        }
+        if (destroy) {
+            this.destroyAllSpritesInRow(yOffset);
+        }
+        else {
+            this.destroyAllSpritesInBlackRow(yOffset);
+            this.createRow(color,yOffset/this.blockSize);
+            this.numBottomColorRows = this.numBottomColorRows + 1;
+        }
+    }
+
+    private destroyAllSpritesInRow(yOffset: number): void {
+        // first make a highlight show around sprites
+        const spritesInRow = this.children.list.filter(spr => spr instanceof Phaser.GameObjects.Sprite).filter(spr => (spr as Phaser.GameObjects.Sprite).y === yOffset);
+        for (const sprite of spritesInRow) {
+            const spriteColor = sprite.getData('color');
+            (sprite as Phaser.GameObjects.Sprite).setTexture(this.getColorName(spriteColor));
+        }
+        // then destroy them
+        setTimeout(() => {
+            for (const sprite of spritesInRow) {
+                sprite.destroy();
+            }
+        }, 300);
+        this.numBlackRows = this.numBlackRows + 1;
+        this.numBottomColorRows = this.numBottomColorRows - 1;
+        if (this.numBottomColorRows === 0) {
+            console.log('YOU WON');
+            this.scene.pause();
+        }
+    }
+
+    private destroyAllSpritesInBlackRow(yOffset: number): void {
+        const spritesInRow = this.children.list.filter(spr => spr instanceof Phaser.GameObjects.Sprite).filter(spr => (spr as Phaser.GameObjects.Sprite).y === yOffset);
+        for (const sprite of spritesInRow) {
+            sprite.destroy();
+        }
+        this.numBlackRows = this.numBlackRows - 1;
+    }
     private getNewColor(y: number): number {
         let colorNumber = 0;
+        /*
+        switch (true) {
+            case y === 0:
+            case y === 1:
+            case y === 2:
+            case y === 3:
+                colorNumber = 0;
+                break;
+            case y === 4:
+            case y === 5:
+            case y === 6:
+            case y === 7:
+                colorNumber = 1;
+                break;
+            case y === 8:
+            case y === 9:
+            case y === 10:
+            case y === 11:
+                colorNumber = 2;
+                break;
+            case y === 12:
+            case y === 13:
+            case y === 14:
+            case y === 15:
+                colorNumber = 3;
+                break;
+            case y === 16:
+            case y === 17:
+            case y === 18:
+            case y === 19:
+                colorNumber = 4;
+                break;
+        }*/
+
         switch (true) {
             case y === 0:
             case y === 1:
@@ -258,8 +347,9 @@ class GameScene extends BaseScene {
                 colorNumber = 9;
                 break;
         }
+
         return colorNumber;
-        
+
     }
     private getColorName(colorNumber: number): string {
         return `color${colorNumber}-highlight`;
@@ -283,27 +373,42 @@ class GameScene extends BaseScene {
 
     private createRows(): void {
         let colorCount = 0;
+
+        // top rows = numGridRows
         for (let y = 0; y < this.numGridRows; y++) {
-            if (y % 2 === 0 && y !== 0) {
+            let numRepeat = 2;
+            if (y % numRepeat === 0 && y !== 0) {
                 colorCount++;
             }
-            // while(x < 2) {
-            for (let x = 0; x < this.numGridCols; x++) {
-                this.spriteGrid.push(
-                    this.add.sprite(x * this.blockSize, y * this.blockSize, `color${colorCount}`).setOrigin(0, 0).setData({color: colorCount})
-                )
-            }
+            this.createRow(colorCount,y);
+        }
 
-            // }
-        }
+        // creates black rows
         for (let y = this.numGridRows; y < this.numBlackRows + this.numGridRows; y++) {
+            
             for (let x = 0; x < this.numGridCols; x++) {
                 this.spriteGrid.push(
-                    this.add.sprite(x * this.blockSize, y * this.blockSize, `piece`).setOrigin(0, 0)
+                    this.add.sprite(x * this.blockSize, y * this.blockSize, `blackbg-sprite`).setOrigin(0, 0).setScale(2)
                 )
             }
         }
-        for (let y = this.numGridRows + this.numBlackRows; y < this.numRandomColorRows + this.numGridRows + this.numBlackRows; y++) {
+
+        // creates bottom rows
+
+        // top rows = numGridRows
+
+        colorCount = 0;
+        for (let y = this.numGridRows + this.numBlackRows; y < this.numBottomColorRows + this.numGridRows + this.numBlackRows; y++) {
+            let numRepeat = 1;
+            //if (y % numRepeat === 0 && y !== 0) {
+            //}
+            this.createRow(colorCount,y);
+            colorCount++;
+
+        }
+
+        /* this was for creating random colors
+        for (let y = this.numGridRows + this.numBlackRows; y < this.numBottomColorRows + this.numGridRows + this.numBlackRows; y++) {
             for (let x = 0; x < this.numGridCols; x++) {
                 const color = Math.floor(Math.random() * this.nbBlockTypes);
                 this.spriteGrid.push(
@@ -311,10 +416,21 @@ class GameScene extends BaseScene {
                 )
             }
         }
+        */
+    }
+
+    private createRow(color: number, yOffset: number): void {
+        console.log(color);
+        console.log(yOffset);
+        for (let x = 0; x < this.numGridCols; x++) {
+            this.spriteGrid.push(
+                this.add.sprite(x * this.blockSize, yOffset * this.blockSize, `color${color}`).setOrigin(0, 0).setData({ color: color }).setScale(2)
+            )
+        }
     }
 
     private addNewPlayerPiece(): void {
-        this.bullet = this.add.sprite(this.player.x, this.player.y, 'bullet').setOrigin(0, 0);
+        this.bullet = this.add.sprite(this.player.x, this.player.y, 'bullet').setOrigin(0, 0).setScale(2);
     }
 
 }
